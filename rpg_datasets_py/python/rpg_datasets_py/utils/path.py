d46c8cfe58bdb9717560815065afbce3f195b908
Titus Cieslewski <dev@titus-c.ch>
2019-07-12 10:03:36 +0200

    license

--- rpg_datasets_py/python/rpg_datasets_py/utils/path.py
+++ rpg_datasets_py/python/rpg_datasets_py/utils/path.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2019 Titus Cieslewski, RPG, University of Zurich, Switzerland
+#   You can contact the author at <titus at ifi dot uzh dot ch>
+# Copyright (C) 2019 Davide Scaramuzza, RPG, University of Zurich, Switzerland
+#
+# This file is part of imips_open_deps.
+#
+# imips_open_deps is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# imips_open_deps is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with imips_open_deps. If not, see <http:#www.gnu.org/licenses/>.
+
 import os
 
 
